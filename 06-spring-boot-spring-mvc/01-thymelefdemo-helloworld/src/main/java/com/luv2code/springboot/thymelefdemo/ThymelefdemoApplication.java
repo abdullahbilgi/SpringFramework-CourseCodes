fd719e8276f3df834431e2245730916c51d060ca
Abdullah Bilgi <abdullahbilgi28@gmail.com>
2023-04-12 17:13:19 +0300

    spring-boot-spring-mvc

---
+++ 06-spring-boot-spring-mvc/01-thymelefdemo-helloworld/src/main/java/com/luv2code/springboot/thymelefdemo/ThymelefdemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymelefdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymelefdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymelefdemoApplication.class, args);
+	}
+
+}
